bc5b257c2bed946110b1f3472802479e1cad40ba
meindokuse <meindokuse@gmai.com>
2024-11-19 23:46:49 +0300

    Change db url to local

--- src/db/database.py
+++ src/db/database.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from src.config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from src.config import DB_USER, DB_PASS, DB_NAME
 
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+asyncpg://${DB_USER}:${DB_PASS}@db/${DB_NAME}"
 Base = declarative_base()
 
 engine = create_async_engine(DATABASE_URL)
